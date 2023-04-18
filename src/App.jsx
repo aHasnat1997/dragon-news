f2af4482ddacd73e0323991903ae09ae366e11d3
aHasnat1997 <hasnat56703@gmail.com>
2023-04-18 13:28:45 +0600

    some basic components add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div>
-      
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 };
